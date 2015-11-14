1c9aa45fb7d8c188ce3aedf94efcbdf1293aadfe
dbond271 <daniel.bond12@yahoo.com>
2015-11-13 22:15:47 -0500

    test commit

--- app/src/main/java/comdormroomdevelopmentdrd2k16/httpsgithub/drd_app30/MainActivity.java
+++ app/src/main/java/comdormroomdevelopmentdrd2k16/httpsgithub/drd_app30/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     //this is too damn hard
-    //
+    ////
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
